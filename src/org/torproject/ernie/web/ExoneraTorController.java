45685869f4a14522a50e287329f85b03c6dab96c
Kevin Berry <xckjb88@gmail.com>
2010-07-23 19:44:39 -0400

    Added ExoneraTorController.

---
+++ src/org/torproject/ernie/web/ExoneraTorController.java
@@ -0,0 +1,104 @@
+package org.torproject.ernie.web;
+
+import java.util.*;
+import java.util.regex.*;
+import java.sql.*;
+import java.sql.Date;
+import java.text.*;
+
+public class ExoneraTorController {
+
+  private Constants c;
+
+  private Connection conn;
+
+  private PreparedStatement psRelays;
+
+  private SimpleDateFormat dfmt;
+
+  private HashSet<String> error;
+
+  private String address;
+
+  private String validAfter;
+
+  private String ipRegex;
+
+  public ExoneraTorController() {
+    this.error = new HashSet<String>();
+    this.dfmt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    try {
+      this.c = new Constants();
+      this.conn = DriverManager.getConnection(c.jdbcURL);
+      this.psRelays= conn.prepareStatement("select * from " +
+          "descriptor_statusentry where address like '?' " +
+          "and validafter >= '?' and validafter <= '?'");
+      this.ipRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])" +
+          "\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+    } catch (SQLException e)  {
+      setError("Internal database error: Could not connect to database");
+    }
+  }
+
+  public void setAddress(String address)  {
+    this.address = address;
+  }
+
+  public void setValidAfter(String validAfter)  {
+    this.validAfter = validAfter;
+  }
+  public Map<String, String> getResults() {
+
+    Map<String, String> relays;
+
+    /* Check if IP address matches regular expression */
+    if (!Pattern.matches("ipRegex", address)) {
+      setError("Enter a valid IP address.");
+    }
+
+    try {
+
+      ResultSet rs;
+      Calendar min = Calendar.getInstance();
+      Calendar max = Calendar.getInstance();
+      min.setTimeZone(TimeZone.getTimeZone("UTC"));
+      max.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+      java.util.Date dValidAfter = dfmt.parse(validAfter);
+      max.setTime(dValidAfter);
+      min = (Calendar)max.clone();
+      min.add(Calendar.HOUR, -3);
+
+      psRelays.setString(1, address);
+      psRelays.setTimestamp(2, min);
+      psRelays.setTimestsmp(3, max);
+
+      rs = psRelays.execute();
+
+      while (rs.next()) {
+        Date validafter;
+        String descriptor;
+        validafter = rs.getTimestamp("validafter");
+        descriptor = rs.getString("descriptor");
+        relays.put(validafter, descriptor);
+      }
+
+      return relays;
+
+    } catch (SQLException e) {
+      setError("Internal database error.");
+    } catch (ParseException e)  {
+      setError("Enter a valid timestamp.");
+    }
+    setError("No results");
+    return relays.put("","");
+  }
+
+  private void setError(String error) {
+    this.error.add(error);
+  }
+
+  public HashSet<String> getError() {
+    return this.error;
+  }
+}
