dc2ad6ca899c564001a50e512a457abe150497e8
Kevin Berry <xckjb88@gmail.com>
2010-12-05 19:51:36 -0500

    Use db pooling with routerdetail and present data.

--- src/org/torproject/ernie/web/RouterDetailServlet.java
+++ src/org/torproject/ernie/web/RouterDetailServlet.java
@@ -1,41 +1,49 @@
 package org.torproject.ernie.web;
 
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+import java.util.logging.*;
+import java.text.*;
+
+import javax.naming.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.*;
-import java.sql.*;
-import java.util.*;
+import javax.sql.*;
 
 public class RouterDetailServlet extends HttpServlet {
 
-  private Connection conn = null;
-  private String connectionURL;
+
+  private DataSource ds;
+
+  private Logger logger;
 
   public void init() {
 
-    /* Try to load the database driver. */
-    try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
-      /* Don't initialize conn and always reply to all requests with
-       * "500 internal server error". */
-      return;
-    }
+    /* Initialize logger. */
+    this.logger = Logger.getLogger(NetworkStatusServlet.class.toString());
 
-    /* Read JDBC URL from deployment descriptor. */
-    connectionURL = getServletContext().
-        getInitParameter("jdbcUrl");
+    /* Look up data source. */
+    try {
+      Context cxt = new InitialContext();
+      this.ds = (DataSource) cxt.lookup("java:comp/env/jdbc/tordir");
+      this.logger.info("Successfully looked up data source.");
+    } catch (NamingException e) {
+      this.logger.log(Level.WARNING, "Could not look up data source", e);
+    }
   }
 
   public void doGet(HttpServletRequest request,
       HttpServletResponse response) throws IOException, ServletException {
 
     String fingerprint;
-    String validafter;
+    java.sql.Timestamp validafter;
 
     try {
       fingerprint = request.getParameter("fingerprint");
-      validafter = request.getParameter("validafter");
+      validafter = new java.sql.Timestamp(
+          Long.parseLong(request.getParameter("validafter")));
+
     } catch (Exception e) {
       response.sendError(HttpServletResponse.SC_NOT_FOUND);
       return;
@@ -45,52 +53,50 @@ public class RouterDetailServlet extends HttpServlet {
         + "FROM statusentry s "
         + "JOIN descriptor d "
         + "ON s.descriptor = d.descriptor "
-        + "WHERE s.fingerprintt = ? "
+        + "WHERE s.fingerprint = ? "
         + "AND s.validafter = ? "
         + "LIMIT 1";
 
     try {
-      conn = DriverManager.getConnection(connectionURL);
+      Connection conn = this.ds.getConnection();
       PreparedStatement ps = conn.prepareStatement(query);
       ps.setString(1, fingerprint);
-      ps.setString(2, validafter);
+      ps.setTimestamp(2, validafter);
       ResultSet rs = ps.executeQuery();
-      if (rs.first()) {
-        request.setAttribute("validafter", rs.getTimestamp(1));
-        request.setAttribute("nickname", rs.getString(2));
-        request.setAttribute("fingerprint", rs.getString(3));
-        request.setAttribute("descriptor", rs.getString(4));
-        request.setAttribute("published", rs.getTimestamp(5));
-        request.setAttribute("address", rs.getString(6));
-        request.setAttribute("orport", rs.getInt(7));
-        request.setAttribute("dirport", rs.getInt(8));
-        request.setAttribute("isauthority", rs.getBoolean(9));
-        request.setAttribute("isbadexit", rs.getBoolean(10));
-        request.setAttribute("isbaddirectory", rs.getBoolean(11));
-        request.setAttribute("isexit", rs.getBoolean(12));
-        request.setAttribute("isfast", rs.getBoolean(13));
-        request.setAttribute("isguard", rs.getBoolean(14));
-        request.setAttribute("ishsdir", rs.getBoolean(15));
-        request.setAttribute("isnamed", rs.getBoolean(16));
-        request.setAttribute("isstable", rs.getBoolean(17));
-        request.setAttribute("isrunning", rs.getBoolean(18));
-        request.setAttribute("isunnamed", rs.getBoolean(19));
-        request.setAttribute("isvalid", rs.getBoolean(20));
-        request.setAttribute("isv2dir", rs.getBoolean(21));
-        request.setAttribute("isv3dir", rs.getBoolean(22));
-        request.setAttribute("version", rs.getString(23));
-        request.setAttribute("bandwidth", rs.getBigDecimal(24));
-        request.setAttribute("ports", rs.getString(25));
-        request.setAttribute("uptime", rs.getBigDecimal(26));
-        request.setAttribute("platform", rs.getString(27));
-      } else {
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        return;
+      if (rs.next()) {
+        request.setAttribute("validafter", rs.getTimestamp("validafter"));
+        request.setAttribute("nickname", rs.getString("nickname"));
+        request.setAttribute("fingerprint", rs.getString("fingerprint"));
+        request.setAttribute("descriptor", rs.getString("descriptor"));
+        request.setAttribute("published", rs.getTimestamp("published"));
+        request.setAttribute("address", rs.getString("address"));
+        request.setAttribute("orport", rs.getInt("orport"));
+        request.setAttribute("dirport", rs.getInt("dirport"));
+        request.setAttribute("isauthority", rs.getBoolean("isauthority"));
+        request.setAttribute("isbadexit", rs.getBoolean("isbadexit"));
+        request.setAttribute("isbaddirectory", rs.getBoolean("isbaddirectory"));
+        request.setAttribute("isexit", rs.getBoolean("isexit"));
+        request.setAttribute("isfast", rs.getBoolean("isfast"));
+        request.setAttribute("isguard", rs.getBoolean("isguard"));
+        request.setAttribute("ishsdir", rs.getBoolean("ishsdir"));
+        request.setAttribute("isnamed", rs.getBoolean("isnamed"));
+        request.setAttribute("isstable", rs.getBoolean("isstable"));
+        request.setAttribute("isrunning", rs.getBoolean("isrunning"));
+        request.setAttribute("isunnamed", rs.getBoolean("isunnamed"));
+        request.setAttribute("isvalid", rs.getBoolean("isvalid"));
+        request.setAttribute("isv2dir", rs.getBoolean("isv2dir"));
+        request.setAttribute("isv3dir", rs.getBoolean("isv3dir"));
+        request.setAttribute("version", rs.getString("version"));
+        request.setAttribute("bandwidth", rs.getBigDecimal("bandwidth"));
+        request.setAttribute("ports", rs.getString("ports"));
+        request.setAttribute("uptime", rs.getBigDecimal("uptime"));
+        request.setAttribute("platform", rs.getString("platform"));
       }
       conn.close();
 
     } catch (SQLException e)  {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      this.logger.log(Level.WARNING, "Error ", e);
       return;
     }
 
